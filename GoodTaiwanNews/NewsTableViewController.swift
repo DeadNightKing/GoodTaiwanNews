c29fd941348d620000577203f91b349234e1b87d
RyanChou <ryanchou@welltend.com.tw>
2022-07-18 17:01:56 +0800

    First pod RxSwift

---
+++ GoodTaiwanNews/NewsTableViewController.swift
@@ -0,0 +1,19 @@
+//
+//  NewsTableViewController.swift
+//  GoodTaiwanNews
+//
+//  Created by Flow_RyanChou on 2022/7/18.
+//
+
+import Foundation
+import UIKit
+import RxSwift
+import RxCocoa
+
+class NewsTableViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+    }
+}
